d1dbde6ffee3445a832004da3cf17a5f88593fc7
Mauricio Costanzi <mauricio.costanzi@gmail.com>
2024-12-15 13:06:06 +0000

    script
    insert script connection to VM 10.211.55.23 in Parallels

---
+++ ssh-curso-dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#-------------------------------------
+# Acesso ssh ao host curso-dev-linux
+# Smartmove - Mauricio - 10.09.24
+#-------------------------------------
+clear
+echo "Connecting host Curso-Dev-Linux..."
+echo
+ssh mauricio@10.211.55.23
+echo
+echo "  ...host desconnected"
+echo
