5f0f87799feada479986959b0bbd12e74852291c
Mauricio Costanzi <93276536+mauriciocostanzi@users.noreply.github.com>
2025-09-17 16:44:11 +0000

    adiciona scripts `lint:check` e ``int:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Teste da Mensagem Ajustada 😃</h1>
+  return <h1>Teste da Mensagem Ajustada 😃</h1>;
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("teste de formatação errada");
 }
 
 export default Home;
